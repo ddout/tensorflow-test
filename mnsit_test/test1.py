d724d3506aae1d15ba777927530f2715e15128ee
ddout@qq.com <ddout@qq.com>
2018-05-11 03:49:09 -0400

    test1

--- mnsit_test/test1.py
+++ mnsit_test/test1.py
@@ -1,13 +1,2 @@
-import tensorflow as tf
 
-hello = tf.constant('Hello, tensorflow!')
-
-sess = tf.Session()
-
-print sess.run(hello)
-
-a = tf.constant(100) 
-
-b = tf.constant(32) 
-
-print sess.run(a + b)
+print 'skdjflskjfl!!!'

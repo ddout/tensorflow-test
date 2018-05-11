b20f3330a8147e63e110ef007245d1bb69e61883
ddout <ddout@qq.com>
2018-05-11 15:42:42 +0800

    3

--- mnsit_test/test1.py
+++ mnsit_test/test1.py
@@ -1,1 +1,13 @@
-print 1
+import tensorflow as tf
+
+hello = tf.constant('Hello, tensorflow!')
+
+sess = tf.Session()
+
+print sess.run(hello)
+
+a = tf.constant(100) 
+
+b = tf.constant(32) 
+
+print sess.run(a + b)
